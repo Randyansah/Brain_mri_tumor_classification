388269ba4e34a3f8c20a1f7031a86d46116fa47b
Randy Kofi Ansah <randyansah97@gmail.com>
2023-09-04 06:36:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+## BRAIN TUMOR CLASSIFICATION
+
+## INTRODUCTION
+### What Is A Tumor
+A tumor is a solid mass of tissue that forms when abnormal cells group together. Tumors can affect bones, skin, tissue, organs and glands. Many tumors are not cancer (they’re benign). But they still may need treatment. Cancerous, or malignant, tumors can be life-threatening and require cancer treatment.
+A tumor is a mass or group of abnormal cells that form in the body. If you have a tumor, it isn’t necessarily cancer. Many tumors are benign (not cancerous).
+
+Tumors can form throughout the body. They can affect bone, skin, tissues, glands and organs. Neoplasm is another word for tumor.
+
+### MRI
+Magnetic resonance imaging, or MRI, is a noninvasive medical imaging test that produces detailed images of almost every internal structure in the human body, including the organs, bones, muscles and blood vessels. MRI scanners create images of the body using a large magnet and radio waves.
+
+### TYPES OF BRAIN TUMOR IN THIS PROJECT
+    GLIOMA:Glioma is a common type of tumor originating in the brain. About 33 percent of all brain tumors are gliomas, which originate in the glial cells that surround and support neurons in the brain, including astrocytes, oligodendrocytes and ependymal cells.
+
+    MENINGIOMA:Meningioma is the most common type of primary brain tumor, accounting for approximately 30 percent of all brain tumors.
+    These tumors originate in the meninges, which are the outer three layers of tissue between the skull and the brain that cover and protect the brain just under the skull.
+    Meningiomas grow out of the middle layer of the meninges called the arachnoid. They grow slowly and may exist for years before being detected. Sometimes doctors will discover a meningioma incidentally on a magnetic resonance imaging (MRI) scan of the head or spinal cord.
+
+    PITUITARY:Pituitary tumors are unusual growths that develop in the pituitary gland. This gland is an organ about the size of a pea. It's located behind the nose at the base of the brain. Some of these tumors cause the pituitary gland to make too much of certain hormones that control important body functions.
+
+
+
+
+
+## OVERVIEW 
+This app will feature a web platform to classify tumors whether they are:
+* GLIOMA
+* MENINGIOMA
+* PITUITARY
+
+## DEPENDENCIES
+The libraries used are in the requirements.txt file
+The various libraries were used in this project.
+* **pandas**
+* **scikit-learn**
+* **transformers**
+* **keras**
+* **tensorflow**
+* **torch**
+* **nltk**
+* **numpy**
+* **opencv-python**
+* **scikit-image**
+* **matplotlib**
+* **gradio**
+
+```
+pip install pandas 
+pip install scikit-learn
+pip install numpy
+pip install nltk
+pip install transformers
+pip install keras
+pip install torch
+pip install tensorflow
+pip install matplotlib
+pip install opencv-python
+pip install scikit-image
+pip install gradio
+```
+
+## PROJECT FILE STRUCTURE
+```sh
+|--.github
+    |--workflows
+        |--main.yml
+|--__init__.py
+|--LICENSE.md
+|--main.py
+|--Makefile
+|--README.md
+|--rewuirements.txt
+```
+
+## AUTHOR
+RANDY KOFI ANSAH
---
+++ main.py
@@ -0,0 +1,115 @@
+from keras.callbacks import EarlyStopping,ReduceLROnPlateau,ModelCheckpoint,TensorBoard,LambdaCallback
+from keras.layers import Input,Dropout, Dense,GlobalAveragePooling2D
+from keras.models import Sequential,Model
+from keras.applications.resnet import ResNet50
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+from tensorflow import keras
+import tensorflow as tf
+from tqdm import tqdm
+import seaborn as sns
+import numpy as np
+import itertools 
+import datetime
+
+import cv2
+import os
+import io
+
+import os 
+
+labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
+
+x_train = [] # training images.
+y_train  = [] # training labels.
+x_test = [] # testing images.
+y_test = [] # testing labels.
+
+image_size = 200
+
+
+for label in labels:
+    trainPath = os.path.join('D:\ML\Dataset\MRI_DATASET\Data\Training',label)
+    for file in tqdm(os.listdir(trainPath)):
+        image = cv2.imread(os.path.join(trainPath, file),0) # load images in gray.
+        image = cv2.bilateralFilter(image, 2, 50, 50) # remove images noise.
+        image = cv2.applyColorMap(image, cv2.COLORMAP_BONE) # produce a pseudocolored image.
+        image = cv2.resize(image, (image_size, image_size)) # resize images into 150*150.
+        x_train.append(image)
+        y_train.append(labels.index(label))
+    
+    testPath = os.path.join('D:\ML\Dataset\MRI_DATASET\Data\Testing',label)
+    for file in tqdm(os.listdir(testPath)):
+        image = cv2.imread(os.path.join(testPath, file),0)
+        image = cv2.bilateralFilter(image, 2, 50, 50)
+        image = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
+        image = cv2.resize(image, (image_size, image_size))
+        x_test.append(image)
+        y_test.append(labels.index(label))
+
+
+x_train = np.array(x_train) / 255.0 # normalize Images into range 0 to 1.
+x_test = np.array(x_test) / 255.0
+
+print(x_train.shape)
+print(x_test.shape)
+
+images = [x_train[i] for i in range(15)]
+fig, axes = plt.subplots(3, 5, figsize = (10, 10))
+axes = axes.flatten()
+for img, ax in zip(images, axes):
+    ax.imshow(img)
+plt.tight_layout()
+plt.show()
+     
+x_train, y_train = shuffle(x_train,y_train, random_state=42) 
+
+y_train = tf.keras.utils.to_categorical(y_train) #One Hot Encoding on the labels
+y_test = tf.keras.utils.to_categorical(y_test)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42) #Dividing the dataset into Training and Validation sets.
+
+print(x_val.shape)     
+
+# ImageDataGenerator transforms each image in the batch by a series of random translations, rotations, etc.
+datagen = ImageDataGenerator(
+    rotation_range=10,                        
+    width_shift_range=0.05,
+    height_shift_range=0.05,
+    horizontal_flip=True)
+
+# After you have created and configured your ImageDataGenerator, you must fit it on your data.
+datagen.fit(x_train)
+
+net = ResNet50(
+    weights='imagenet', # Load weights pre-trained on ImageNet.
+     include_top=False, # Do not include the ImageNet classifier at the top.
+     input_shape=(image_size,image_size,3))
+
+model = net.output
+model = GlobalAveragePooling2D()(model)
+model = Dropout(0.4)(model)
+model = Dense(4, activation="softmax")(model)
+model = Model(inputs= net.input, outputs= model)
+
+#compile our model.
+adam = keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=adam, loss = 'categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+model.fit(x_train,y_train, epochs=5,batch_size=32,validation_data=(x_val,y_val))
+
+model.save(".\MRI_brain.keras")
+model.load_weights(".\MRI_brain.keras")
+
+mri_image=("./Te-no_0011.jpg")
+img = tf.keras.utils.load_img(mri_image, target_size=(200, 200))
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)
+
+y_pred=model.predict(img_array)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
