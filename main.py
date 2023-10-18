28f7c90a5932560044700497d714f005959e8f0e
Randyansah <randyansah97@gmail.com>
2023-10-18 14:20:55 +0000

    Corrected code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from lib.predict import predict
 def main():
     x_train,y_train,x_val,y_val=load_dataset('./Data/Training','./Data/Testing')
     train_model(x_train,y_train,x_val,y_val)
-    predict("MRI_brain.keras")
+    predict("./Te-no_0011.jpg")
 
 if __name__=='__main__':
     main()
