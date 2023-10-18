94c204dba1fd300b77dce0c7177e32053a938790
Randyansah <randyansah97@gmail.com>
2023-10-18 14:59:49 +0000

    Skrr skrr

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib.predict import predict
 
 
 def main():
-    x_train,y_train,x_val,y_val=load_dataset('./Data/Training','./Data/Testing')
+    x_train,x_val,x_test,y_train,y_val,y_test=load_dataset('./Data/Training','./Data/Testing')
     train_model(x_train,y_train,x_val,y_val,200)
     predict('MRI_brain.keras',"./","Te-no_0011.jpg")
 
